ce99b65e91b5e3a3ca728a4e2ed7286a277c3e4d
Daniil <sidorchuk.daniil@gmail.com>
2022-09-28 21:52:04 +0300

    Commit 4. Add script to change color of sidepanel after scroll to white color screen

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,20 @@
 addEventListener('DOMContentLoaded', () => {
     const hamburger = document.querySelector('.hamburger'),
           menu = document.querySelector('.menu'),
-          closeElem = document.querySelector('.menu__close');
+          closeElem = document.querySelector('.menu__close'),
+          sidePanel = document.querySelector('.sidepanel');
 
     hamburger.addEventListener('click', () => {
         menu.classList.add('active');
     });
     closeElem.addEventListener('click', () => {
         menu.classList.remove('active');
+    });
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 385) {
+            sidePanel.classList.add('sidepanel_dark');
+        } else {
+            sidePanel.classList.remove('sidepanel_dark');
+        }
     });
 });
